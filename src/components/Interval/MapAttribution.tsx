eabffc88a607cd40c6bf07f858bc9c2bd53e06a4
Ryan Hanna <36575799+ryanphanna@users.noreply.github.com>
2026-07-07 00:34:36 -0400

    fix: keep map attribution on one line (nowrap, wider pill cap)

--- src/components/Interval/MapAttribution.tsx
+++ src/components/Interval/MapAttribution.tsx
@@ -4,8 +4,8 @@ import { MAP_BADGE, MAP_BADGE_LABEL, Z_PANEL } from '../../styles';
 export function MapAttribution() {
   return (
     <div className={`absolute bottom-6 left-6 ${Z_PANEL} pointer-events-auto`}>
-      <div className={`${MAP_BADGE} h-8 max-w-[min(18rem,calc(100vw-6rem))]`}>
-        <p className={`${MAP_BADGE_LABEL} leading-none [&_a]:text-[var(--text-dim)] [&_a]:no-underline [&_a]:hover:text-[var(--text-primary)]`}>
+      <div className={`${MAP_BADGE} h-8 max-w-[calc(100vw-3rem)]`}>
+        <p className={`${MAP_BADGE_LABEL} whitespace-nowrap leading-none [&_a]:text-[var(--text-dim)] [&_a]:no-underline [&_a]:hover:text-[var(--text-primary)]`}>
           Map tiles by{' '}
           <a href="https://carto.com/attributions" target="_blank" rel="noopener noreferrer">CARTO</a>
           , under CC BY 3.0. Data by{' '}
