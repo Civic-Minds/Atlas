0f1a61c2ae0974c3875bee96026edfc518c622cb
Ryan Hanna <36575799+ryanphanna@users.noreply.github.com>
2026-07-07 00:34:46 -0400

    fix: attribution links use normal weight, not bold

--- src/components/Interval/MapAttribution.tsx
+++ src/components/Interval/MapAttribution.tsx
@@ -5,7 +5,7 @@ export function MapAttribution() {
   return (
     <div className={`absolute bottom-6 left-6 ${Z_PANEL} pointer-events-auto`}>
       <div className={`${MAP_BADGE} h-8 max-w-[calc(100vw-3rem)]`}>
-        <p className={`${MAP_BADGE_LABEL} whitespace-nowrap leading-none [&_a]:text-[var(--text-dim)] [&_a]:no-underline [&_a]:hover:text-[var(--text-primary)]`}>
+        <p className={`${MAP_BADGE_LABEL} whitespace-nowrap leading-none [&_a]:font-normal [&_a]:text-[var(--text-dim)] [&_a]:no-underline [&_a]:hover:text-[var(--text-primary)]`}>
           Map tiles by{' '}
           <a href="https://carto.com/attributions" target="_blank" rel="noopener noreferrer">CARTO</a>
           , under CC BY 3.0. Data by{' '}
